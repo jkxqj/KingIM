7cffedc72fa5ea93851de9bc29fe032d030db3c5
JKXQJ <jinkaixqj@gmail.com>
2019-01-12 00:00:12 +0800

    fix

--- src/main/java/kingim/ws/FriendWS.java
+++ src/main/java/kingim/ws/FriendWS.java
@@ -1,7 +1,5 @@
 package kingim.ws;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,20 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
-import kingim.model.Friend;
-import kingim.model.FriendType;
-import kingim.model.Group;
 import kingim.model.User;
 import kingim.service.FriendService;
 import kingim.service.FriendTypeService;
 import kingim.service.GroupUserService;
 import kingim.service.UserService;
 import kingim.utils.RedisUtils;
-import kingim.vo.SNSFriend;
-import kingim.vo.SNSGroup;
-import kingim.vo.SNSInit;
-import kingim.vo.SNSUser;
-import kingim.vo.SNSdata;
 
 @Controller
 @RequestMapping("api/friend")
@@ -105,92 +95,6 @@ public class FriendWS {
 	public String yunpanSharePage(HttpSession session,Model model) {
 		return "js/plugins/layim/yunpanShare";
 	}	
-	
-	
-	/*
-	 * @author jack 
-	 * 给新版聊聊提供初始化数据服务，包括个人信息、好友列表信息、群组列表信息
-	 * @param userId
-	 */
-	@RequestMapping(value = "getInitList", produces = "text/plain; charset=utf-8")
-	public @ResponseBody String getInitList(int userId) {
-		 User u=userService.getUserById(userId);
-		 SNSUser mine=new SNSUser();
-		 mine.setId(u.getId());
-		 if(u.getAvatar()==null || u.getAvatar().equals("")){
-			 mine.setAvatar("images/avatar/default120.png");
-		 }else{
-			 mine.setAvatar(u.getAvatar());
-		 }
-		 mine.setSign(u.getSign());
-		 mine.setUsername(u.getNickName());
- 	 	//获取redis中的用户在线状态
- 		String redisKey=userId+"_status";
- 		RedisUtils.set(redisKey, "online");
- 		mine.setStatus("online");
-		SNSInit snsinit =new SNSInit();
-		SNSdata data=new SNSdata();
-		data.setMine(mine);
-		snsinit.setData(data);
-		List<SNSFriend> snsFriendList = new ArrayList<>();
-		List<FriendType> list = friendTypeService.getFriendTypeByUserId(userId);
-		if(list.size()==0){
-			int id=friendTypeService.getByUserId(userId);  //如果没有默认分组，则初始化分组
-			SNSFriend snsFriend = new SNSFriend();
-			snsFriend.setGroupname("好友");
-			snsFriend.setOnline(0);
-			snsFriend.setId(id);
-			snsFriendList.add(snsFriend);
-		}else{
-			try{
-				for(int i=0;i<list.size();i++){
-					SNSFriend snsFriend = new SNSFriend();
-					snsFriend.setGroupname(list.get(i).getTypeName());
-					snsFriend.setId(list.get(i).getId());
-					List<Friend> friendList=list.get(i).getFriends();
-					List<SNSUser> snsUserList = new ArrayList<SNSUser>();
-					int onlineNum=0;
-					for(int j=0;j<friendList.size();j++){
-						SNSUser snsUser = new SNSUser();
-						snsUser.setAvatar(friendList.get(j).getFriendInfo().getAvatar());
-						snsUser.setSign(friendList.get(j).getFriendInfo().getSign());
-						snsUser.setUsername(friendList.get(j).getFriendInfo().getNickName());
-						snsUser.setId(friendList.get(j).getFriendId());
-						onlineNum++;
-			    	 	//获取redis中的用户在线状态
-			    		redisKey=friendList.get(j).getFriendId()+"_status";
-			    		if(RedisUtils.exists(redisKey)){
-			    			snsUser.setStatus(RedisUtils.get(redisKey).toString());
-			    		}else{
-			    			snsUser.setStatus("offline");
-			    		}
-						snsUserList.add(snsUser);
-					}
-					snsFriend.setOnline(onlineNum);
-					snsFriend.setList(snsUserList);
-					snsFriendList.add(snsFriend);
-				}
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-		}
-
-		data.setFriend(snsFriendList);
-		//获取我加入的群的列表
-		List<Group> groupList = groupUserService.getByUserId(userId);
-		List<SNSGroup> glist = new ArrayList<SNSGroup>();
-		for(int k=0;k<groupList.size();k++){
-			SNSGroup sgroup = new SNSGroup();
-			sgroup.setGroupname(groupList.get(k).getGroupName());
-			sgroup.setId(groupList.get(k).getId());
-			sgroup.setAvatar(groupList.get(k).getAvatar());
-			glist.add(sgroup);
-		}
-		data.setGroup(glist);
-		snsinit.setData(data);
-		return JSON.toJSONString(snsinit);
-	}
-
 
 	
 }
